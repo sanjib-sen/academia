afbab888a2b961d3ca405fe779c79c118d5e3e5f
sanjib-sen <sksenonline@gmail.com>
2021-07-27 18:06:19 +0600

    lab02 done cse420

--- CSE420/Lab02/main.py
+++ CSE420/Lab02/main.py
@@ -1,7 +1,7 @@
-inp = input("Enter The Address: ")
+
 
 allowed = ['-','_','.']
-
+special_double = ["@@","!!","##","$$","%\%","^^","&&","**","||","??",">>","<<","::",";;",",,","--","==","++","__","``"]
 def isNumber(character):
     return True if ord(character) > 47 and ord(character)<58 else False
 
@@ -10,22 +10,58 @@ def isLetter(character):
 
 def allowedDomainChars(string):
     for s in string:
-        if not isNumber(s) or isLetter(s) or '-': return False
+        if not isNumber(s) and (not isLetter(s)) and (s!='-') and (s!='.'): return False
     return True
-
-def isEmail(string):
-    return True if '@' in string else False
 
 def isValidUserName(string):
     return False if len(string)>64 or string=="" or " " in string or isNumber(string[0]) or string[0] == '.' or string[-1] == '.' or '..' in string else True
 
 def isValidEmailDomain(string):
-    string = str(string)
-    if len(string)>255 or string=="" or " " in string or string[0] == '-' or string[-1] == '-' or '@' in string or '..' in string or '.' not in string or string[string.index('.')+1]=='-': return False
+    if len(string)>255 or string=="" or " " in string or '..' in string or '.' not in string or string[string.index('.')+1]=='-': return False
+    dotsplit = string.split(".")
+    for dot in dotsplit:
+        if dot[0]=='-' or dot[-1]=='-':return False 
     return True if allowedDomainChars(string) else False
 
-def isvalidEmail(string):
+def isValidEmail(string):
+    if not '@' in string or '@@' in string: return False
     stringList = string.split("@")
-    if not len(stringList)<=2: return False
-    elif not isValidUserName(stringList[0]): return False
-    elif not isValidEmailDomain(stringList[1]): return False
+    if len(stringList)>2: 
+        return False
+    if not isValidUserName(stringList[0]): 
+        return False
+    if not isValidEmailDomain(stringList[1]): 
+        return False
+    return True
+
+def isValidWeb(string):
+    if " " in string or ".." in string: return False
+    string = string.split(".")
+    if len(string)<3: return False
+    if string[0] != "https://www" and string[0]!= "http://www" and string[0]!= "www": return False
+    for s in string:
+        if s[0]=='-' or s[-1]=='-': return False
+        for spc in special_double:
+            if spc in s:
+                return False
+    return True
+
+
+def main(inp, num):
+    if isValidWeb(inp):
+        print("Web,",num)
+    elif isValidEmail(inp):
+        print("Email,",num)
+
+
+cases = int(input())
+lst =[]
+for i in range(1,cases+1):
+    inp = input()
+    lst.append([inp,i])
+
+for l in lst:
+    main(l[0],l[1])
+
+
+
